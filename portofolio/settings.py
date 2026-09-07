7a7236759fa1d472349f376d10b5bcab8714ca78
IlmanMaman <mail.ilmanghani@gmail.com>
2026-09-07 17:52:29 +0700

    Fix ALLOWED_HOSTS typo in settings.py

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ALLOWED_HOST = ["localhost", "127.0.0.1", "ilman-ghani-myportofolio.pws.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ilman-ghani-myportofolio.pws.cs.ui.ac.id"]
 
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
